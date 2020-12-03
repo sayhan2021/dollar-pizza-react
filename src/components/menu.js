fcae498229087821612585a2ba2b01fb2b54b9bc
sayhan2021 <schowdhury0250@richmondhillhs.org>
2020-12-03 18:32:13 +0000

    pizza

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,6 +3,8 @@ import Plain from './plain';
 import Pepperoni from './pepperoni';
 import MeatLover from './meat-lover';
 import Veggie from './veggie';
+import Hawaiian from './hawaiian';
+import Bbq from './bbq';
 import './css/menu.css';
 
 function Menu() {
@@ -11,10 +13,13 @@ function Menu() {
       <h1 className='primary-text'>New York's World Famous</h1>
       <h1 className='secondary-text'>Pizza Menu</h1>
       <div className='row'>
+
         <Plain/>
         <Pepperoni/>
         <MeatLover/>
         <Veggie/>
+        <Hawaiian/>
+        <Bbq/>
       </div>
     </div>
   );
--- src/components/plain.js
+++ src/components/plain.js
@@ -9,7 +9,7 @@ function Plain() {
         <img src={ plain } class="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">Plain Slice</h5>
-          <p className="card-text">It's got cheese, it's got tomato sauce, and it's only  a dollar. What more could you ask for?</p>
+          <p className="card-text">Its got cheese, its got tomato, and its only a dollar. What else could you ask for.</p>
         </div>
         <ul className="list-group list-group-flush">
           <li className="list-group-item">Price: $1.00</li>
